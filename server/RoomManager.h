dcf18ad9129d953bb86a7e6776a98ebe616eef88
Matteo Serva <matteo.serva@gmail.com>
2013-02-05 14:05:11 +0100

    efficiency fix

--- server/RoomManager.h
+++ server/RoomManager.h
@@ -1,7 +1,7 @@
 
 #ifndef ROOMMANAGER_H
 #define ROOMMANAGER_H
-#include <vector>
+#include <list>
 
 
 namespace ygo {
@@ -17,7 +17,7 @@ namespace ygo {
         static void keepAlive(evutil_socket_t fd, short events, void* arg);
         public:
         event_base* net_evbase;
-            std::vector<CMNetServer *> elencoServer;
+                std::list<CMNetServer *> elencoServer;
         GameServer* gameServer;
         void setGameServer(ygo::GameServer*);
 
