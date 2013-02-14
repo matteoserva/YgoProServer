558c76104164d6b4a28632bce634868411497225
Matteo Serva <matteo.serva@gmail.com>
2013-02-14 14:01:44 +0100

    conservative programming

--- server/GameServer.cpp
+++ server/GameServer.cpp
@@ -136,7 +136,15 @@ void GameServer::ServerEchoEvent(bufferevent* bev, short events, void* ctx)
         DuelPlayer* dp = &(that->users[bev]);
         DuelMode* dm = dp->game;
         if(dp->netServer)
-            dp->netServer->LeaveGame(dp);
+            {
+                dp->netServer->LeaveGame(dp);
+                if(that->users.find(bev)!= that->users.end())
+                {
+                    log(BUG,"BUG: tcp terminated but disconnectplayer not called\n");
+                    that->DisconnectPlayer(dp);
+                }
+
+            }
         else that->DisconnectPlayer(dp);
     }
 }
