d92cb84c675f48889890588d3290e2a078322318
Matteo Serva <matteo.serva@gmail.com>
2013-09-26 17:07:24 +0200

    fix

--- server/WaitingRoomFrontEnd.cpp
+++ server/WaitingRoomFrontEnd.cpp
@@ -88,7 +88,7 @@ void WaitingRoom::ShowStats(DuelPlayer* dp)
     switch (dp->loginStatus)
     {
     case Users::LoginResult::AUTHENTICATED:
-        swprintf(wmessage,20, L"Score: %d:",score);
+        swprintf(wmessage,20, L"Score: %d, rank:?",score);
         SendNameToPlayer(dp,2,wmessage);
         sprintf(message, "www.ygopro.it");
         //sprintf(message, "Score: %d(%+d)",score,dp->cachedGameScore-score);
